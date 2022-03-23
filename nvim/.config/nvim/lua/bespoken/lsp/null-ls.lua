46d5cad72622c88f505d6eaf52d72e91f44b9c8d
mr_bliss <devan.mcgeer@zenaptix.com>
2022-03-23 12:02:55 +0200

    Highlight shortcut made capital

--- nvim/.config/nvim/lua/bespoken/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/bespoken/lsp/null-ls.lua
@@ -7,13 +7,20 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-    formatting.fixjson,
+		formatting.prettier.with({
+			extra_args = { "--single-quote", "--jsx-single-quote" },
+			prefer_local = "node_modules/.bin",
+		}),
+		formatting.fixjson,
 		formatting.stylua,
+		diagnostics.eslint_d.with({
+			prefer_local = "node_modules/.bin",
+		}),
+		code_actions.eslint_d,
 	},
 })
-
