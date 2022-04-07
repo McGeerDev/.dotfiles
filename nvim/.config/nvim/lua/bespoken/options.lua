1524c78d4945b5fe7ebd9cf4a33efa85a1b3632e
mr_bliss <devan.mcgeer@zenaptix.com>
2022-04-07 12:44:27 +0200

    Autoread=true

--- nvim/.config/nvim/lua/bespoken/options.lua
+++ nvim/.config/nvim/lua/bespoken/options.lua
@@ -1,4 +1,5 @@
 local options = {
+  autoread = true,
   backup = false,                          -- creates a backup file
   clipboard = { "unnamedplus", "unnamed" },-- allows neovim to access the system clipboard
   cmdheight = 2,                           -- more space in the neovim command line for displaying messages
