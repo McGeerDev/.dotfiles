1d47380af9fbedebbe5497d06cfd2c88ac96bbec
dmcgeer <devan@rezco.co.za>
2024-02-14 12:59:41 +0200

    Updated installsh to include error checking

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 pushd .
-for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+
+# Check if STOW_FOLDERS variable is set
+if [ -z $STOW_FOLDERS ]; then
+    echo "Error: STOW_FOLDERS variable is not set."
+    exit 1
+fi
+
+pushd . || { echo "Error: Unable to push directory onto the stack."; exit 1; }
+for folder in $(echo "$STOW_FOLDERS" | sed "s/,/ /g")
 do
-    stow -D $folder
-    stow $folder
+    stow -D $folder || { echo "Error: Unable to unstow $folder."; popd; exit 1; }
+    stow $folder || { echo "Error: Unable to stow $folder."; popd; exit 1; }
 done
-popd
+popd || { echo "Error: Unable to pop directory off the stack."; exit 1; }
