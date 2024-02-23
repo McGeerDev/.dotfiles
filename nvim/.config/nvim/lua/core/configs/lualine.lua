4f9120e7383a91fa12437dad0fce85ba6e505b7f
dmcgeer <devan@rezco.co.za>
2024-02-23 09:45:58 +0200

    Added safe require for lualine
    Added mode and branch visibility to lualine

--- nvim/.config/nvim/lua/core/configs/lualine.lua
+++ nvim/.config/nvim/lua/core/configs/lualine.lua
@@ -1,18 +1,36 @@
-require('lualine').setup {
+-- use pcall to safely require lualine
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+  print("Could not load lualine", lualine)
+  return
+end
+
+lualine.setup {
   options = {
     icons_enabled = true,
     component_separators = '|',
     section_separators = '',
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
   },
   sections = {
     lualine_x = {
       {
         require("noice").api.statusline.mode.get,
         cond = require("noice").api.statusline.mode.has,
-        color = { fg = "#ff9e64" },
+        color = { fg = "#ff9e64" }
       }
     },
     lualine_a = {
+      {'mode'},
+    },
+    lualine_b = {
+      {'branch', 'diff','diagnostics'},
+    },
+    lualine_c = {
       {
         'buffers',
       }
