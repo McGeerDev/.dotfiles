b9284222d89292f349e91a97b6c8966e1a10092e
mcgeerdev <mcgeer.devan@gmail.com>
2024-02-24 13:23:42 +0200

    Added autopairs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -19,6 +19,10 @@ Kickstart.nvim is a template for your own configuration.
   - https://neovim.io/doc/user/lua-guide.html
 --]]
 
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
