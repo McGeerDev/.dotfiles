84419af6c0d50cbc3219b3f9c39ce6f195fd47bf
Devan McGeer <devan@meshtrade.co>
2025-07-18 00:05:31 +0200

    feat(wezterm): Added dark and light modes
    Added tab to bottom of window
    Added time to tab bar
    Added toast on config update
    Refactored to single config object

--- nvim/.config/wezterm/wezterm.lua
+++ nvim/.config/wezterm/wezterm.lua
@@ -9,69 +9,112 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.automatically_reload_config = true
+wezterm.on("update-right-status", function(window, _)
+	local date = wezterm.strftime("%d-%a %H:%M:%S")
 
--- For example, changing the color scheme:
-config.color_scheme = "Google (light) (terminal.sexy)"
---config.color_scheme = 'Google Light (Gogh)'
---config.color_scheme = "nord-light"
---config.color_scheme = "Homebrew Light (Gogh)"
---config.color_scheme = "Papercolor Light (Gogh)"
+	-- Make it italic and underlined
+	window:set_right_status(wezterm.format({
+		{ Attribute = { Underline = "Single" } },
+		{ Attribute = { Italic = true } },
+		{ Text = date },
+	}))
+end)
+
+wezterm.on("window-config-reloaded", function(window, _)
+	window:toast_notification("wezterm", "configuration reloaded!", nil, 4000)
+end)
+
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "tokyonight_night"
+	else
+		return "dayfox"
+	end
+end
+
+wezterm.on("window-config-reloaded", function(window, _)
+	local overrides = window:get_config_overrides() or {}
+	local appearance = window:get_appearance()
+	local scheme = scheme_for_appearance(appearance)
+	if overrides.color_scheme ~= scheme then
+		overrides.color_scheme = scheme
+		window:set_config_overrides(overrides)
+	end
+end)
+
+config = {
+	check_for_updates = true,
+	automatically_reload_config = true,
+	default_cursor_style = "SteadyBar",
+
+	-- For example, changing the color scheme:
+	--color_scheme = "Google (light) (terminal.sexy)",
+	--color_scheme = 'Google Light (Gogh)'
+	--color_scheme = "nord-light"
+	--color_scheme = "Homebrew Light (Gogh)"
+	--color_scheme = "Papercolor Light (Gogh)"
+	--color_scheme = "dayfox",
+
+	-- Tab settings
+	hide_tab_bar_if_only_one_tab = true,
+	enable_tab_bar = true,
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = true,
+	show_tab_index_in_tab_bar = false,
+
+	window_background_opacity = 0.85,
+	macos_window_background_blur = 10,
+	window_close_confirmation = "AlwaysPrompt",
+
+	-- What does this line even do?
+	scrollback_lines = 3000,
+	inactive_pane_hsb = {
+		saturation = 0.30,
+		brightness = 0.7,
+	},
+	window_decorations = "RESIZE",
+	window_padding = {
+		left = "2cell",
+		right = "2cell",
+		top = "0cell",
+		bottom = "0cell",
+	},
+	window_frame = {
+		border_left_width = "0.75px",
+		border_right_width = "0.75px",
+		border_bottom_height = "0.5px",
+		border_top_height = "0.5px",
+	},
+	--font = wezterm.font("Fira Code"),
+	font = wezterm.font("Fira Mono", { weight = "Bold" }),
+	font_size = 16.0,
+
+	-- keys
+	keys = {
+		-- Turn off the default CMD-m Hide action, allowing CMD-m to
+		-- be potentially recognized and handled by the tab
+		{ key = "m", mods = "CMD", action = act.DisableDefaultAssignment },
+		-- Clears the scrollback and viewport leaving the prompt line the new first line.
+		{ key = "k", mods = "CMD", action = act.ClearScrollback("ScrollbackAndViewport") },
+		-- Move between panes
+		{ key = "l", mods = "SHIFT|CMD", action = act.ActivatePaneDirection("Right") },
+		{ key = "h", mods = "SHIFT|CMD", action = act.ActivatePaneDirection("Left") },
+		{ key = "k", mods = "SHIFT|CMD", action = act.ActivatePaneDirection("Up") },
+		{ key = "j", mods = "SHIFT|CMD", action = act.ActivatePaneDirection("Down") },
+		-- Resolve this spliting pane issue
+		{ key = "-", mods = "SHIFT|CMD", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		-- SHIFT is for when caps lock is on
+		{ key = "|", mods = "SHIFT|CMD", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "x", mods = "SHIFT|CMD", action = act.CloseCurrentPane({ confirm = true }) },
+		-- Reset the font size to 16px
+		{ key = "0", mods = "CMD", action = act.ResetFontSize },
+	},
+}
 
 -- Tab settings
-config.hide_tab_bar_if_only_one_tab = true
-
--- Window settings
-config.window_background_opacity = 0.85
-config.macos_window_background_blur = 10
-
-config.window_close_confirmation = "AlwaysPrompt"
-config.scrollback_lines = 3000
 config.default_workspace = "home"
-
 -- Dim inactive panes
-config.inactive_pane_hsb = {
-	saturation = 0.30,
-	brightness = 0.7,
-}
-
-config.window_decorations = "RESIZE"
-config.window_padding = {
-	left = "2cell",
-	right = "2cell",
-	top = "0cell",
-	bottom = "0cell",
-}
-config.window_frame = {
-	border_left_width = "0.75px",
-	border_right_width = "0.75px",
-	border_bottom_height = "0.5px",
-	border_top_height = "0.5px",
-}
-
 -- Font
-config.font = wezterm.font("Fira Code")
-config.font_size = 16.0
-
-config.keys = {
-	-- Turn off the default CMD-m Hide action, allowing CMD-m to
-	-- be potentially recognized and handled by the tab
-	{ key = "m", mods = "CMD", action = act.DisableDefaultAssignment },
-	-- Clears the scrollback and viewport leaving the prompt line the new first line.
-	{ key = "k", mods = "CMD", action = act.ClearScrollback("ScrollbackAndViewport") },
-	-- Move between panes
-	{ key = "l", mods = "SHIFT|CMD", action = act.ActivatePaneDirection("Right") },
-	{ key = "h", mods = "SHIFT|CMD", action = act.ActivatePaneDirection("Left") },
-	{ key = "k", mods = "SHIFT|CMD", action = act.ActivatePaneDirection("Up") },
-	{ key = "j", mods = "SHIFT|CMD", action = act.ActivatePaneDirection("Down") },
-	-- Resolve this spliting pane issue
-	{ key = "-", mods = "SHIFT|CMD", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	-- SHIFT is for when caps lock is on
-	{ key = "|", mods = "SHIFT|CMD", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "x", mods = "SHIFT|CMD", action = act.CloseCurrentPane({ confirm = true }) },
-	-- Reset the font size to 16px
-	{ key = "0", mods = "CMD", action = act.ResetFontSize },
-}
 
 -- and finally, return the configuration to wezterm
 return config
